16cd794d095f6ac4beb7a3a38595a482e20e48e0
pedrorudi1 <pedro_rudi@hotmail.com>
2024-09-13 14:25:09 -0300

    Atualizada e aprimorada

--- calculadora.py
+++ calculadora.py
@@ -13,37 +13,27 @@ def menu():
     return input(menu)
     
     
-def soma(input1, input2, solution):
-    solution = input1 + input2
-    return solution
+def soma(input1, input2):
+    return input1 + input2
 
 
-def subtracao(input1, input2, solution):
-    solution = input1 - input2
-    return solution
+def subtracao(input1, input2):
+    return input1 - input2
 
 
-def multiplicacao(input1, input2, solution):
-    solution = input1 * input2
-    return solution
+def multiplicacao(input1, input2):
+    return input1 * input2
 
 
-def divisao_inteiros(input1, input2, solution, resto):
-    solution = input1 // input2
-    resto = input1 % input2
-    return solution, resto
+def divisao_inteiros(input1, input2):
+    return input1 // input2, input1 % input2
 
 
-def divisao(input1, input2, solution):
-    solution = input1 / input2
-    return solution
+def divisao(input1, input2):
+    return input1 / input2
 
 
 def main():
-    input1 = 0
-    input2 = 0
-    solution = 0
-    resto = 0
 
     while True:
 
@@ -52,31 +42,31 @@ def main():
         if opcao == "1":
             input1 = float(input('Digite o primeiro número da operação: '))
             input2 = float(input('Digite o segundo número da equação: '))
-            solution = soma(input1, input2, solution)
+            solution = soma(input1, input2)
             print(f'O resultado da soma é: {solution}')
 
         elif opcao == "2":
             input1 = float(input('Digite o primeiro número da operação: '))
             input2 = float(input('Digite o segundo número da equação: '))
-            solution = subtracao(input1, input2, solution)
+            solution = subtracao(input1, input2)
             print(f'O resultado da subtração é: {solution}')
 
         elif opcao == "3":
             input1 = float(input('Digite o primeiro número da operação: '))
             input2 = float(input('Digite o segundo número da equação: '))
-            solution = multiplicacao(input1, input2, solution)
+            solution = multiplicacao(input1, input2)
             print(f'O resultado da multiplicação é: {solution}')
 
         elif opcao == "4":
             input1 = int(input('Digite o primeiro número da operação: '))
             input2 = int(input('Digite o segundo número da equação: '))
-            solution, resto = divisao_inteiros(input1, input2, solution, resto)
+            solution, resto = divisao_inteiros(input1, input2)
             print(f'O resultado da divisão é: {solution}. O resto da operação é: {resto}.')
 
         elif opcao == "5":
             input1 = float(input('Digite o primeiro número da operação: '))
             input2 = float(input('Digite o segundo número da equação: '))
-            solution = divisao(input1, input2, solution)
+            solution = divisao(input1, input2)
             print(f'O resultado da divisão é: {solution}')
 
         elif opcao == "6":
